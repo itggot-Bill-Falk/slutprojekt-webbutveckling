bcb1a61d63290f82360c384d85f4d11de91b38bc
itggot-Bill-Falk <bill.falk@itggot.se>
2018-05-21 19:50:22 +0200

    Har uppdaterat sidan med bilder samt en del mediaqueries

--- Infopage/site/js/code.js
+++ Infopage/site/js/code.js
@@ -2,3 +2,16 @@
 //     var element = document.querySelector(".first-button__blurb");
 //     element.classList.toggle("toggleTextShit");
 // }
+
+var width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var height = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+if (width >= 322) {
+    document.getElementById("pokemon").src = "img/mediumpachirisu.png"
+    document.getElementById("stats").src = "img/mediumstats.png"
+}
